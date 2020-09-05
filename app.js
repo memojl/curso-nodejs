e6422162bee610cf71edbd3d782935f4721d4088
memo-azell <memo.jimenez@azell.co>
2020-09-04 20:27:47 -0500

    produccion

--- app.js
+++ app.js
@@ -2,23 +2,23 @@ const express = require('express');
 const app = express();
 
 const port = 3000;
+
 // Motor de plantillas
 app.set('view engine', 'ejs');
 app.set('views', __dirname+'/views');
-
 
 
 app.use(express.static(__dirname + "/public"));
 
 app.get('/',(req, res)=>{
   //res.send('Mi respuesta desde express');
-  res.render('index',{titulo: 'Mi titulo dinamico'})
-})
+  res.render('index',{titulo: 'Mi titulo dinamico'});
+});
 
 app.get('/servicios',(req, res)=>{
   //res.send('Mi respuesta desde servicios');
-  res.render('servicios',{titulo: 'Este es un mensaje dinamico de la pagina de sercios'})
-})
+  res.render('servicios',{titulo: 'Este es un mensaje dinamico de la pagina de sercios'});
+});
 
 app.use((req,res,next)=>{
   //res.status(404).sendFile(__dirname + '/public/404.html');
@@ -28,9 +28,16 @@ app.use((req,res,next)=>{
   });
 });
 
+/*
 app.listen(port, ()=>{
   console.log('Servidor a su servicio en el puerto', port);
 })
+*/
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Our app is running on port ${ PORT }`);
+});
 
 /*SERVIDOR NODE JS
 const http = require('http');
