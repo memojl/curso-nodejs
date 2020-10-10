0d3b7dbcae806e0d02768539887c8671a77abb6a
memo-azell <memo.jimenez@azell.co>
2020-10-10 16:18:10 -0500

    update 10.10.2020

--- router/Mascotas.js
+++ router/Mascotas.js
@@ -23,4 +23,63 @@ router.get('/', async (req, res)=>{
 });
 
 
+//FORMULARIO
+router.get('/crear', (req, res)=>{
+    res.render('crear')
+})
+
+//GUARDAR
+router.post('/', async(req, res)=>{
+    const body = req.body;
+    console.log(body); 
+    try {
+        await Mascota.create(body)
+        res.redirect('/mascotas')
+    } catch (error) {
+        console.log('error', error)
+    }   
+})
+
+//EDITAR
+router.get('/:id', async(req, res)=>{
+    const id = req.params.id;
+    try {
+        const mascotaDB = await Mascota.findOne({ _id:id })//console.log(mascotaDB)
+        res.render('detalle', {
+            mascota: mascotaDB,
+            error: false
+        })
+    } catch (error) {
+        console.log(error)
+        res.render('detalle', {
+            error: true,
+            mensaje: 'No se encuentra el id selecionado'
+        })
+    }
+})
+
+//ELIMINAR
+router.delete('/:id', async(req,res)=>{
+    const id = req.params.id;
+    try {
+        const mascotaDB = await Mascota.findByIdAndDelete({ _id:id })
+        if(mascotaDB){
+            res.json({
+                estado: true,
+                mensaje: 'eliminado!'
+            })
+        }else{
+            res.json({
+                estado: false,
+                mensaje: 'Fallo eliminado!'
+            })
+        }
+
+    } catch (error) {
+        console.log(error)
+    }
+
+})
+
+
 module.exports = router;
