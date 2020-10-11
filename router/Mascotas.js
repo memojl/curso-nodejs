f970bc2a0d39f28a127512629d234f49f9ae6c15
memo-azell <memo.jimenez@azell.co>
2020-10-10 23:12:59 -0500

    update Editar

--- router/Mascotas.js
+++ router/Mascotas.js
@@ -81,5 +81,23 @@ router.delete('/:id', async(req,res)=>{
 
 })
 
+//Editar
+router.put('/:id', async(req,res)=>{
+    const id = req.params.id
+    const body = req.body
+
+    try {
+        const mascotaDB = await Mascota.findByIdAndUpdate(id, body, {useFindAndModify: false})
+        console.log(mascotaDB)
+        
+        res.json({
+            estado: true,
+            mensaje: 'Editado'
+        })
+        
+    } catch (error) {
+        console.log(error)
+    }
+})
 
 module.exports = router;
